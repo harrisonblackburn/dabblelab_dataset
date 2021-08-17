d94310493c47adf270c4d8c56c180f5f748216d0
harrisonblackburn <blackbhr@yahoo.com>
2021-08-17 12:13:15 -0500

    schema, resolvers, typedef set up

---
+++ index.js
@@ -0,0 +1,57 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Albania" type defines the queryable fields for every year in our data source.
+  type Albania {
+    year: String
+    population: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+const albania = [
+    {
+      year: '2000',
+      population: '3401198'
+    },
+    {
+      year: '2001',
+      population: '3073734'
+    },
+    {
+      year: '2002',
+      population: '3093465'
+    },
+    {
+      year: '2003',
+      population: '3111162'
+    },
+    
+  ];
+
+  // Resolvers define the technique for fetching the types defined in the
+// schema. 
+const resolvers = {
+    Query: {
+      albania: () => albania,
+    },
+  };
+  
+  // The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ schema.sql
@@ -0,0 +1,26 @@
+[{
+    "Country": "Albania",
+    "Year": "2000",
+    "Area (square kilometres)": 28748, 
+    "Total Population": 3401198
+},
+[{
+    "Country": "Albania",
+    "Year": "2001",
+    "Area (square kilometres)": 28748, 
+    "Total Population": 3073734
+},
+[{
+    "Country": "Albania",
+    "Year": "2002",
+    "Area (square kilometres)": 28748, 
+    "Total Population": 3093465
+},
+[{
+    "Country": "Albania",
+    "Year": "2003",
+    "Area (square kilometres)": 28748, 
+    "Total Population": 3111162
+},
+
+]
